d3d8f2465ed7a38056b6ede304b185d0388950ad
Emiliano Prediger <ej.prediger@gmail.com>
2020-07-26 14:19:44 -0300

    lógica de negocio en capa de servicio, bdd roto

--- app/main/controller/clientes_controller.py
+++ app/main/controller/clientes_controller.py
@@ -1,96 +1,59 @@
-from datetime import datetime, timedelta
 
 from flask import jsonify, request
 from flask.blueprints import Blueprint
-from pytz import timezone
 
-from main.db.database import (agregar_instancia, editar_instancia, eliminar_instancia,
-                      obtener_instancias_por_filtro,
-                      obtener_todas_las_instancias, obtener_una_instancia)
-from main.models.Cliente import Cliente
-from main.models.Ticket import Ticket
-from main.settings import (CODIGO_HTTP_BAD_REQUEST, CODIGO_HTTP_NO_CONTENT,
-                      CODIGO_HTTP_NOT_FOUND, CODIGO_HTTP_OK, SEVERIDADES)
+from main.service.clientes_service import (obtener_clientes as obtener_clientes_service,
+                                           crear, editar, obtener_cliente_por, eliminar)
+from main.settings import (CODIGO_HTTP)
 
 clientes = Blueprint('clientes', __name__)
 
 
 @clientes.route('/clientes', methods=['GET'])
 def obtener_clientes():
-	clientes = obtener_todas_las_instancias(Cliente)
-	todos_los_clientes = [c.a_diccionario() for c in clientes]
-	return jsonify(todos_los_clientes), CODIGO_HTTP_OK
+	respuesta = obtener_clientes_service()
+	return jsonify(respuesta), CODIGO_HTTP["OK"]
 
 @clientes.route('/clientes/<int:id>', methods=['GET'])
 def obtener_cliente(id):
 	try:
-		cliente = obtener_una_instancia(Cliente, id=id)
-		return jsonify(cliente.a_diccionario()), CODIGO_HTTP_OK
+		cliente = obtener_cliente_por(id)
+		return jsonify(cliente), CODIGO_HTTP["OK"]
 	except:
-		return jsonify({'mensaje': 'Cliente no encontrado'}), CODIGO_HTTP_BAD_REQUEST
+		return jsonify({'mensaje': 'Cliente no encontrado'}), CODIGO_HTTP["BAD_REQUEST"]
 
 @clientes.route('/clientes', methods=['POST'])
 def crear_cliente():
 	try:
 		data = request.get_json()
-		razon_social = data['razon_social']
-		descripcion = data['descripcion']
-		CUIT = data['CUIT']
-		#fecha_desde_que_es_cliente = data['fecha_desde_que_es_cliente']
+		for d in data:
+			if data[d] == None:
+				return jsonify({'mensaje': 'Parametros invalidos'}), CODIGO_HTTP["BAD_REQUEST"]
+
+		cliente = crear(data)
+
+		return jsonify(cliente), CODIGO_HTTP["CREATED"]
 	except:
-		return jsonify({'mensaje': 'Parametros invalidos'}), CODIGO_HTTP_BAD_REQUEST
-
-	for d in data:
-		if data[d] == None:
-			return jsonify({'mensaje': 'Parametros invalidos'}), CODIGO_HTTP_BAD_REQUEST
-
-	fecha_creacion = datetime.now(timezone('America/Argentina/Buenos_Aires'))
-
-	c = agregar_instancia(Cliente,
-						razon_social=razon_social,
-						descripcion=descripcion,
-						CUIT=CUIT,
-						fecha_desde_que_es_cliente=fecha_creacion)
-	cliente_diccionario = c.a_diccionario()
-
-	return jsonify(cliente_diccionario), CODIGO_HTTP_OK
+		return jsonify({'mensaje': 'Parametros invalidos'}), CODIGO_HTTP["BAD_REQUEST"]
 
 @clientes.route('/clientes/<int:id>', methods=['PUT'])
 def editar_cliente(id):
 	try:
 		data = request.get_json()
-		razon_social = data['razon_social']
-		descripcion = data['descripcion']
-		CUIT = data['CUIT']
-		estado = data['estado']
-		#fecha_desde_que_es_cliente = data['fecha_desde_que_es_cliente']
+		for d in data:
+			if data[d] == None:
+				return jsonify({'mensaje': 'Parametros invalidos'}), CODIGO_HTTP["BAD_REQUEST"]
+
+		editar(id, data)
+
+		return jsonify(), CODIGO_HTTP["NO_CONTENT"]
 	except:
-		return jsonify({'mensaje': 'Parametros invalidos'}), CODIGO_HTTP_BAD_REQUEST
-
-	for d in data:
-		if data[d] == None:
-			return jsonify({'mensaje': 'Parametros invalidos'}), CODIGO_HTTP_BAD_REQUEST
-
-	editar_instancia(Cliente, id, razon_social=razon_social,
-					descripcion=descripcion,
-					CUIT=CUIT,
-					estado=estado
-					#fecha_desde_que_es_cliente=fecha_desde_que_es_cliente
-					)
-
-	return jsonify(), CODIGO_HTTP_NO_CONTENT
+		return jsonify({'mensaje': 'Parametros invalidos'}), CODIGO_HTTP["BAD_REQUEST"]
 
 @clientes.route('/clientes/<int:id_cliente>', methods=['DELETE'])
 def eliminar_cliente(id_cliente):
-	cliente = obtener_una_instancia(Cliente, id=id_cliente)
-
-	if not cliente:
-		return jsonify({'mensaje': 'No existe el cliente solicitado'}), CODIGO_HTTP_NOT_FOUND
-
-	ticket_asociado = obtener_una_instancia(Ticket, id_cliente=id_cliente)
-
-	if ticket_asociado:
-	 	return jsonify({'mensaje': 'No se puede eliminar el cliente solicitado ya que está asignado a un ticket'}), CODIGO_HTTP_BAD_REQUEST
-
-	eliminar_instancia(Cliente, id=id_cliente)
-	return jsonify({'mensaje': 'Cliente eliminado con exito!'}), CODIGO_HTTP_OK
+	try:
+		eliminar(id_cliente)
+		return jsonify({'mensaje': 'Cliente eliminado con exito!'}), CODIGO_HTTP["OK"]
+	except Exception as e:
+		return jsonify({'mensaje': str(e)}), CODIGO_HTTP["NOT_FOUND"]
---
+++ app/main/service/tareas_service.py
@@ -0,0 +1,15 @@
+from main.models.TicketTarea import TicketTarea
+from main.service.tickets_service import obtener_ticket_por
+
+def crear_tarea(tarea, id_ticket):
+	ticket = obtener_ticket_por(id=id_ticket)
+	if not ticket:
+		raise Exception('No existe el ticket solicitado')
+
+	id_tarea = tarea['id_tarea']
+	id_proyecto = tarea['id_proyecto']
+
+	agregar_instancia(TicketTarea,
+					id_ticket=id_ticket,
+					id_tarea=id_tarea,
+					id_proyecto=id_proyecto)
