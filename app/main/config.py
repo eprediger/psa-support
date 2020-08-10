1d92501c68e0a3a9660b2d46c79090f881bd08de
Ramaa <ramaa196@gmail.com>
2020-08-09 23:14:30 -0300

    variable de base de datos postgres

--- app/main/config.py
+++ app/main/config.py
@@ -1,5 +1,5 @@
 from decouple import config
-
+import os
 
 class Config:
     pass
@@ -19,7 +19,7 @@ class FeatureTestingConfig(Config):
 
 class ProductionConfig(Config):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = config('DATABASE_URL', default='localhost')
+    SQLALCHEMY_DATABASE_URI = 'postgres://odgquqttvlwybb:ff7f6c568bd3bbe6efa4514ec3fe892350f7a4386f1f1d2fd26ce6acda5dbd7e@ec2-52-204-20-42.compute-1.amazonaws.com:5432/d998qs20a58aqc'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
