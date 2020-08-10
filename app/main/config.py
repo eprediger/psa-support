a3cc2b650f63805e8ba7b851c9b441010b86c524
Ramaa <ramaa196@gmail.com>
2020-08-09 23:56:51 -0300

    comento lugares donde esta sqlite

--- app/main/config.py
+++ app/main/config.py
@@ -7,19 +7,19 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+#    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 
 class FeatureTestingConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///'
+#    SQLALCHEMY_DATABASE_URI = 'sqlite:///'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 
 class ProductionConfig(Config):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = 'postgres://odgquqttvlwybb:ff7f6c568bd3bbe6efa4514ec3fe892350f7a4386f1f1d2fd26ce6acda5dbd7e@ec2-52-204-20-42.compute-1.amazonaws.com:5432/d998qs20a58aqc'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
