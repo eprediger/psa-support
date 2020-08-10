aa860cdd8a506dfb131053c3089b766074f25946
Emiliano Prediger <ej.prediger@gmail.com>
2020-08-10 01:57:58 -0300

    prints

--- app/main/service/tickets_service.py
+++ app/main/service/tickets_service.py
@@ -145,17 +145,25 @@ def archivar(id):
 	eliminar_instancia(Ticket, id=id)
 
 def completar_ceros(tickets):
+	print(tickets)
+
 	if len(tickets) > 0:
 		fechaInicio = tickets[0]["fecha"]
 		fechaFin = date.today()
 		delta = fechaFin - fechaInicio
 
+		print(fechaInicio)
+		print(fechaFin)
+
 		for i in range(delta.days + 1):
 			dia = fechaInicio + timedelta(days=i)
+			print(dia)
 			diaString = dia.strftime("%Y-%m-%d")
-			if diaString not in [ ticket['fecha'].strftime("%Y-%m-%d") for ticket in tickets ]:
+			print(diaString)
+			if diaString not in [ ticket['fecha'] for ticket in tickets ]:
 				tickets.append({'fecha': diaString, 'cantidad': 0})
 
+		print(tickets)
 		tickets.sort(key=lambda x: x["fecha"])
 
 	return tickets
